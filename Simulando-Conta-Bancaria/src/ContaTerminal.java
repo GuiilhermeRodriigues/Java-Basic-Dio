5c77257d513802ec8d528a37361b1287f56a3a34
Guilherme Rodrigues <gui.rodrigues786@gmail.com>
2024-05-28 01:58:16 -0400

    Projeto completo

--- Simulando-Conta-Bancaria/src/ContaTerminal.java
+++ Simulando-Conta-Bancaria/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite seu nome !");
+        String nome = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Agencia !");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta !");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite seu saldo !");
+        float saldo = scanner.nextFloat();
+        
+
+        System.out.println("Olá" + nome +  ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo R$ " + saldo +  " já está disponível para saque.");
+
+        scanner.close();
+
     }
 }
